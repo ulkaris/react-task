3167a00e252fa2c61dbe0094148f7f18d3c1d2f8
ulkaris <ulkerisabalayeva80@gmail.com>
2025-11-11 18:19:38 +0400

    Modal opening error is solved

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   optimizeDeps: {
-    include: ["react-quilljs", "quill"], // <-- burada react-quilljs əlavə olunur
+  optimizeDeps: {
+    include: ["react-quilljs", "quill", "lodash.isequal"], // <-- bunu əlavə et
   },
-})
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true, // <-- bu CJS modulları üçün lazımdır
+    },
+  },
+});
